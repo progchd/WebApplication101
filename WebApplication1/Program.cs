b72b220f3caa40d76a8373b4458249a1e6bfa75f
progchd <progchd@gmail.com>
2026-06-29 16:24:31 +0530

    demo

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -17,7 +17,8 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "cool-demo1"
+    , "cool-demo1", "cool-demo1", "cool-demo1"
 };
 
 app.MapGet("/weatherforecast", () =>
