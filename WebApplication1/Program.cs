504a90b255c363953c5bea668bce61ab9a16d1d6
progchd <progchd@gmail.com>
2026-06-29 15:58:21 +0530

    dsadsad

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
